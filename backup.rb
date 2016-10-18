fdc18b5c1bc1256db428fe99174dfd964b490f9a
max pleaner <maxpleaner@gmail.com>
2016-10-18 14:30:11 -0700

    2016-10-18 14:30:11 -0700

--- backup.rb
+++ backup.rb
@@ -1,4 +1,14 @@
-# define Backup
+
+# To get backup to work, you need to take a couple steps first:
+
+# 1. make a folder: <browser_tester root>/../browser_scripts
+# that it to say, it's one level above the project root
+
+# 2. Run 'git init' in that folder and add an origin remote pointing to some
+# repo you own. 
+
+# 3. Then you can run Backup.run
+
 class Backup
   def self.run
     script = <<-SH
